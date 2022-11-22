c60b6a6ee44c89f9b1d93d5ff5071efae934f494
Hyeonjong Jeong <hyejeong@c6r10s1.42seoul.kr>
2022-11-23 01:30:20 +0900

    11/23 01:30

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,36 +6,41 @@
 /*   By: hyejeong <hyejeong@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 11:40:34 by hyejeong          #+#    #+#             */
-/*   Updated: 2022/11/21 11:40:46 by hyejeong         ###   ########.fr       */
+/*   Updated: 2022/11/23 00:50:28 by hyejeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*str;
-	int		s1_len;
-	int		s2_len;
-	int		i;
-	int		j;
+	char			*temp;
+	unsigned int	s_len;
+	unsigned int	i;
 
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
-	str = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
+	s_len = ft_strlen(s);
+	if (start > s_len)
+		s_len = 0;
+	else
+	{
+		if (len > s_len - start)
+			s_len = s_len - start;
+		else
+			s_len = len;
+	}
+	temp = (char *)malloc(sizeof(char) * (s_len + 1));
+	if (!temp)
+		return (0);
 	i = 0;
-	while (s1[i] != '\0')
+	while (i < s_len)
 	{
-		str[i] = s1[i];
+		temp[i] = s[start + i];
 		i++;
 	}
-	j = 0;
-	while (s2[j] != '\0')
-	{
-		str[i] = s2[j];
-		j++;
-		i++;
-	}
-	str[i] = '\0';
-	return (str);
+	temp[i] = '\0';
+	return (temp);
 }
+/*
+주어진 문자열의 특정 인덱스(start)부터 특정 사이즈(len) 만큼의 문자들을 동적할당된 메모리에 저장합니다.
+주의할점은 메모리 낭비를 방지하기 위해 여러가지 케이스로 나누어 메모리 할당량을 정확히 계산해 내는것입니다.
+*/
