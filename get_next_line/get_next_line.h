108f12cc160df57e13c226df1cf03b2e71207d9c
Hyeonjong Jeong <hyejeong@c6r6s1.42seoul.kr>
2022-12-12 15:54:33 +0900

    12/12

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyejeong <hyejeong@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/12 15:26:56 by hyejeong          #+#    #+#             */
+/*   Updated: 2022/12/12 15:28:50 by hyejeong         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
