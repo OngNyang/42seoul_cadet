8b5f9d9fd09dcf62aa97e6fb099f454fa635c553
jhj951105@gmail.com <jhj951105@gmail.com>
2022-12-15 12:00:00 +0900

    12/15

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -2,15 +2,33 @@
 #include <stdio.h>
 #include "get_next_line.h"
 
-int main(void)
+// int main(void)
+// {
+// 	int fd;
+
+// 	fd = 0;
+// 	fd = open("./test.txt", O_RDONLY);
+// 	char *line = get_next_line(fd);
+// 	printf("%p\n", line);
+// 	printf("%s", line);
+
+// 	return (0);
+// }
+
+int	main(void)
 {
-	int fd;
+	int		temp;
+	int		fd;
+	char	*line;
 
-	fd = 0;
-	fd = open("./test.txt", O_RDONLY);
-	char *line = get_next_line(fd);
-	printf("%p\n", line);
-	printf("%s", line);
-
+	fd = open("test.txt", O_RDONLY);
+	while ((temp = (get_next_line(fd, &line)) > 0))
+	{
+		printf("%s\n", line);
+		free(line);
+	}
+	printf("%s\n", line);
+	free(line);
+	close(fd);
 	return (0);
 }
