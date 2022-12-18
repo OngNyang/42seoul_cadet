38561bad06c5226b349f792777286928206b0f79
jhj951105@gmail.com <jhj951105@gmail.com>
2022-12-18 13:56:58 +0900

    12/18

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -10,10 +10,89 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
-#define BUFFER_SIZE 1024
+// #include "get_next_line.h"
+// #define BUFFER_SIZE 1024
 
-static char *ft_read_line(int fd, char *buf, char *backup)
+// static char *ft_read_line(int fd, char *buf, char *backup)
+// {
+// 	int		count;
+// 	char	*temp_pointer;
+
+// 	count = 1;
+// 	while (count)
+// 	{
+// 		count = read(fd, buf, BUFFER_SIZE);
+// 		if (count == -1)
+// 			return (0);
+// 		else if (count == 0)
+// 			break ;
+// 		buf[count] = '\0';
+// 		if (!backup)
+// 			backup = ft_strdup("");
+// 		temp_pointer = backup;
+// 		backup = ft_strjoin(temp_pointer, buf);
+// 		if (!backup)
+// 			return (NULL);
+// 		free(temp_pointer);
+// 		temp_pointer = NULL;
+// 		if (ft_strchr(buf, '\n'))
+// 			break ;
+// 	}
+// 	return (backup);
+// }
+
+// static char *ft_extract(char *line)
+// {
+// 	int	i;
+// 	char	*result;
+
+// 	i = 0;
+// 	while (line[i] != '\n' && line[i] != '\0')
+// 		i++;
+// 	if (line [i] == '\0')
+// 		return (0);
+// 	result = ft_substr(line, i+1, ft_strlen(line) - i);
+// 	if (!result)
+// 		return (NULL);
+// 	if (result[0] == '\0')
+// 	{
+// 		free(result);
+// 		result = NULL;
+// 		return (NULL);
+// 	}
+// 	line[i + 1] = '\0';
+// 	return (result);
+// }
+
+// char	*get_next_line(int fd)
+// {
+// 	char	*line;
+// 	char	*buf;
+// 	static char	*backup;
+
+// 	if (fd < 0 || BUFFER_SIZE <= 0)
+// 		return (NULL);
+// 	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+// 	if (!buf)
+// 		return (NULL);
+// 	line = ft_read_line(fd, buf, backup);
+// 	free(buf);
+// 	buf = NULL;
+// 	if (!line)
+// 		return (NULL);
+// 	backup = ft_extract(line);
+// 	return (line);
+// }
+
+
+
+
+
+
+#include "get_next_line.h"
+#define BUFFER_SIZE 4
+
+static char	*ft_read_line(int fd, char *buf, char *backup)
 {
 	int		count;
 	char	*temp_pointer;
@@ -21,25 +100,29 @@ static char *ft_read_line(int fd, char *buf, char *backup)
 	count = 1;
 	while (count)
 	{
-		count = read(fd, buf, BUFFER_SIZE);
-		if (count == -1)
+		count = read(fd, buf, BUFFER_SIZE); //버퍼사이즈 만큼 읽어 들이는데, 읽어 들인 사이즈를 저장해줌.
+		if (count == -1) //read()는 읽기에 실패했을때 -1을 리턴함.
 			return (0);
-		else if (count == 0)
+		else if(count ==0)
 			break ;
-		buf[count] = '\0';
+		buf[count] = '\0'; //널 터미네이팅
 		if (!backup)
-			backup = ft_strdup("");
+			backup = ft_strdup(""); //backup이 비어있다면, 아무것도 없는 문자열을 만들어준다.
 		temp_pointer = backup;
-		backup = ft_strjoin(temp_pointer, buf);
+		backup = ft_strjoin(temp_pointer, buf); //버퍼와 backup 문자열을 융합해준다.
 		if (!backup)
 			return (NULL);
-		free(temp_pointer);
-		temp_pointer = NULL;
-		if (ft_strchr(buf, '\n'))
+		free(temp_pointer); //임시로 사용한 메모리 해제해줌.
+		temp_pointer = NULL; //임시 포인터 변수 다시 초기화 해줌.
+		if (ft_strchr(buf, '\n')) //개행을 만나면 멈춘다.
 			break ;
 	}
 	return (backup);
 }
+//버퍼 사이즈가 개행을 만나기 전에 끝난다면 -> while 문 반복
+//버퍼 사이즈가 개행을 1개만 담고 다음 개행 이저에 끝난다면 -> while문 break. 
+//버퍼 사이즈가 전문을 다 담는다면 -> while문 break.
+//버퍼 사이즈가 개행을 못만나면, 반복해서 읽기를 한다.
 
 static char *ft_extract(char *line)
 {
@@ -47,39 +130,40 @@ static char *ft_extract(char *line)
 	char	*result;
 
 	i = 0;
-	while (line[i] != '\n' && line[i] != '\0')
+	while (line[i] != '\n' && line[i] != '\0') // \n이나  \0을 만날때까지 인덱스 전진
 		i++;
-	if (line [i] == '\0')
+	if (line[i] == '\0')
 		return (0);
-	result = ft_substr(line, i+1, ft_strlen(line) - i);
+	result = ft_substr(line, i + 1, ft_strlen(line) - i); //해당 범위 만큼 동적할당 뒤 복사해준다. 
 	if (!result)
 		return (NULL);
-	if (result[0] == '\0')
+	if (result[0] == '\0') //복사한 문자열 널가드
 	{
 		free(result);
 		result = NULL;
 		return (NULL);
 	}
-	line[i + 1] = '\0';
+	line[i + 1] = '\0'; //널 터미네이팅.
 	return (result);
 }
+//추출된 문자열 중에서, \0 이나 \n을 만나기 전까지의 문자들만 추출하여 반환한다.
 
 char	*get_next_line(int fd)
 {
-	char	*line;
-	char	*buf;
+	char		*line;
+	char		*buf;
 	static char	*backup;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0 || BUFFER_SIZE <= 0) //fd와 버퍼사이즈 유효성 검사.
 		return (NULL);
-	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1)); //버퍼 메모리 동적할당
 	if (!buf)
 		return (NULL);
-	line = ft_read_line(fd, buf, backup);
-	free(buf);
+	line = ft_read_line(fd, buf, backup); //버퍼 사이즈만큼 읽어서 저장한다. 개행을 만날때까지 해야 한다.
+	free(buf); //버퍼 비워준다.
 	buf = NULL;
 	if (!line)
 		return (NULL);
-	backup = ft_extract(line);
+	backup = ft_extract(line); //주어진 문자열 중에서 \0, \n을 만나기 전까지의 문자들만 추출하여 저장.
 	return (line);
 }
--- get_next_line/main.c
+++ get_next_line/main.c
@@ -2,18 +2,6 @@
 #include <stdio.h>
 #include "get_next_line.h"
 
-// int main(void)
-// {
-// 	int fd;
-
-// 	fd = 0;
-// 	fd = open("./test.txt", O_RDONLY);
-// 	char *line = get_next_line(fd);
-// 	printf("%p\n", line);
-// 	printf("%s", line);
-
-// 	return (0);
-// }
 
 int main(void)
 {
@@ -29,3 +17,46 @@ int main(void)
 	}
 	return (0);
 }
+
+// #define BUFFER_SIZE 1024
+
+// static char	*ft_read_line(int fd, char *buf, char *backup)
+// {
+// 	int		count;
+
+// 	count = read(fd, buf, BUFFER_SIZE); //버퍼사이즈 만큼 읽어 들이는데, 읽어 들인 사이즈를 저장해줌.
+// 	printf("%d", count);
+// 	return (backup);
+// }
+
+// char	*get_next_line(int fd)
+// {
+// 	char		*line;
+// 	char		*buf;
+// 	static char	*backup;
+
+// 	if (fd < 0 || BUFFER_SIZE <= 0) //fd와 버퍼사이즈 유효성 검사.
+// 		return (NULL);
+// 	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1)); //버퍼 메모리 동적할당
+// 	if (!buf)
+// 		return (NULL);
+// 	line = ft_read_line(fd, buf, backup);
+// 	return (line);
+// }
+
+
+// int main(void)
+// {
+// 	int		fpointer;
+// 	char	*singleLine = malloc(1 * sizeof(char));
+
+// 	fpointer = open("./test.txt", 256);
+// 	get_next_line(fpointer);
+// 	// while (singleLine != NULL)
+// 	// {
+// 	// 	free(singleLine);
+// 	// 	singleLine = get_next_line(fpointer);
+// 	// 	printf("%s", singleLine);
+// 	// }
+// 	return (0);
+// }
