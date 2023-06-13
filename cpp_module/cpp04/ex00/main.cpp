f6a5ddbb265b6885261980df65665255f4519a3e
Hyeonjong Jeong <hyejeong@cx1r3s2.42seoul.kr>
2023-06-13 14:13:39 +0900

    commit

--- cpp_module/cpp04/ex00/main.cpp
+++ cpp_module/cpp04/ex00/main.cpp
@@ -34,21 +34,41 @@ class	Dog :public Animal
 Animal::Animal(void)
 {
 	this->type = "Animal";
+	std::cout << this->type << " Animal class is constructed by default." << std::endl;
 }
 
 Animal::Animal(const Animal& obj)
 {
 	if (this == &obj)
+	{
+		std::cout << "Copy constructor for itself. refused." << std::endl;
 		return ;
+	}
 	else
 	{
 		this->type = obj.type;
+		std::cout << this->type << " Animal class is copy-constructed." << std::endl;
 	}
 }
 
 Animal&	Animal::operator=(const Animal& obj)
 {
-	
+	if (this == &obj)
+	{
+		std::cout << "Copy assignment for itself. refused." << std::endl;
+		return (*this);
+	}
+	else
+	{
+		this->type = obj.type;
+		std::cout << "Copy assignment "
+		return (*this);
+	}
+}
+
+Animal::~Animal(void)
+{
+
 }
 
 
