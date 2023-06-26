9dc5f93707023badc3e406e8fd131afcede0786b
Hyeonjong Jeong <hyejeong@cx1r3s4.42seoul.kr>
2023-06-26 14:52:33 +0900

    commit

--- cpp_module/cpp02/ex01/main.cpp
+++ cpp_module/cpp02/ex01/main.cpp
@@ -13,21 +13,17 @@ int main( void )
 	Fixed const c( 42.42f );
 	Fixed const d( b );
 
-	// int	n = -2147483648;
-	int	n = 2147483647;
-	a = Fixed(n);
-	std::cout << a << std::endl;
 
-	// a = Fixed( 1234.4321f );
+	a = Fixed( 1234.4321f );
 
-	// std::cout << "a is " << a << std::endl;
-	// std::cout << "b is " << b << std::endl;
-	// std::cout << "c is " << c << std::endl;
-	// std::cout << "d is " << d << std::endl;
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
 
-	// std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-	// std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-	// std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-	// std::cout << "d is " << d.toInt() << " as integer" << std::endl;
-	// return 0;
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	return 0;
 }
